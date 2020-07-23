e67f286d922e02066bbe3cc4dfedafaf37577f3c
Rehoboth Okorie <okorie2021@gmail.com>
2020-07-23 11:05:30 -0400

    urlen_Added_GitRepo_Link_V2

--- static/shorten/js/git.js
+++ static/shorten/js/git.js
@@ -1,5 +1,9 @@
 $(document).ready(function myFunction() {
   $("#gitRepo").click(() => {
-    console.log("Hello World")
+    var copyText = document.getElementById("gitHolder");
+    console.log("")
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
   })
 })
